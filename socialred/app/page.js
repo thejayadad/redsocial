b0f4682c7e25d1f9752bc865031abcc91a989413
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-07-06 20:01:41 -0400

    update

---
+++ socialred/app/page.js
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main>
+      <h2>Hello World</h2>
+    </main>
+  )
+}
---
+++ socialred/models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  posts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }],
+  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
